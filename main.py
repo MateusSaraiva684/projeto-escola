83ccd5b022dd208db39582ee5050f2616f554bb0
Mateus Saraiva <mateussaraiva684@gmail.com>
2026-03-21 15:36:41 -0300

    fix rota home

--- main.py
+++ main.py
@@ -5,15 +5,17 @@ from app.routes import alunos
 from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
+
+# 🔹 Static (CSS, imagens)
 app.mount("/static", StaticFiles(directory="app/static"), name="static")
+
+# 🔹 Criar tabelas
 Base.metadata.create_all(bind=engine)
+
+# 🔹 Rotas
 app.include_router(alunos.router, prefix="/alunos")
 
+# 🔹 Rota inicial (abre a página web)
 @app.get("/")
 def home():
     return RedirectResponse(url="/alunos/web")
-
-@app.get("/")
-def home():
-    return {"msg": "API funcionando"}
-    
